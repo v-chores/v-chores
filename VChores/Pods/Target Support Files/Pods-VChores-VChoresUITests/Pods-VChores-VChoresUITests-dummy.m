987ecc86ad74cca868d7f4885f338abb219b14a2
John Lim <johnlim0043@gmail.com>
2022-04-22 19:40:25 -0400

    added pod file

---
+++ VChores/Pods/Target Support Files/Pods-VChores-VChoresUITests/Pods-VChores-VChoresUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VChores_VChoresUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VChores_VChoresUITests
+@end
---
+++ VChores/Pods/Target Support Files/Pods-VChores/Pods-VChores-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VChores : NSObject
+@end
+@implementation PodsDummy_Pods_VChores
+@end
