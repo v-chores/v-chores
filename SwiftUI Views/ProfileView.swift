943767ca25d315617a8f368d00f1ac35753b615b
klaireklaire <khlinph@gmail.com>
2022-05-07 22:59:27 -0400

    Added ProfileView in SwiftUI

---
+++ SwiftUI Views/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  VChores
+//
+//  Created by klaire ✨ on 5/7/22.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
--- VChores/AccountViewController.swift
+++ VChores/AccountViewController.swift
@@ -6,24 +6,27 @@
 //
 
 import UIKit
+import SwiftUI
 
 class AccountViewController: UIViewController {
 
+    @IBOutlet weak var profileContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let controller = UIHostingController(rootView: ProfileView())
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addChild(controller)
+        self.view.addSubview(controller.view)
+        controller.didMove(toParent: self)
+        
+        NSLayoutConstraint.activate([
+            controller.view.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+            controller.view.heightAnchor.constraint(equalTo: self.view.heightAnchor),
+            controller.view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            controller.view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
